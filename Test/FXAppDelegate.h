f216f595f7e1cef407edcdaf9af70ebdc0a2243d
XuBingoo <251311016@qq.com>
2014-05-23 19:50:50 +0800

    Initial Commit

---
+++ Test/FXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FXAppDelegate.h
+//  Test
+//
+//  Created by 徐宝桥 on 14-5-23.
+//  Copyright (c) 2014年 ___MyCompanyName___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
